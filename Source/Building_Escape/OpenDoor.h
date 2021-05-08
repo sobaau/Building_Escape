d03e9bf252860dad2821e04e2d95890defa493b5
Soba <15152226+sobaau@users.noreply.github.com>
2021-05-08 23:56:01 +1000

    Initial commit

---
+++ Source/Building_Escape/OpenDoor.h
@@ -0,0 +1,40 @@
+// Copyright David Preston 2021
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    void TriggerOpen(const float DeltaTime);
+private:
+    float InitialYaw;
+    float CurrentYaw;
+
+    UPROPERTY(EditAnywhere)
+    float TargetYaw = 90.f;
+
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+
+    UPROPERTY(EditAnywhere)
+    AActor* ActorThatOpens = nullptr;
+};
